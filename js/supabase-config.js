abf396104c73c4db41936ba15c80f7ab05aad090
Akkhilkapoor <hiitambalaskillclass@gmail.com>
2026-07-25 08:30:38 +0530

    supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = "अपना Project URL यहाँ डालो";
+const SUPABASE_URL = "https://ukkckxyoalvjtwxrrhzs.supabase.co";
 
-const SUPABASE_ANON_KEY = "अपनी Publishable Key यहाँ डालो";
+const SUPABASE_ANON_KEY = "sb_publishable_OSbtu8ZI8P_pJUPAVb1o3Q_ZtKVm1vL";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
