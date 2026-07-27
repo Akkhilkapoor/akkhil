89f50163fa1778a59f981cc4de15f526c454725c
Akkhilkapoor <hiitambalaskillclass@gmail.com>
2026-07-27 14:35:53 +0530

    supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -6,3 +6,15 @@ const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
+const SUPABASE_URL =
+    "https://ukkckxyoalvjtwxrrhzs.supabase.co";
+
+const SUPABASE_ANON_KEY =
+    "sb_publishable_OSbtu8ZI8P_pJUPAVb1o3Q_ZtKVm1vL";
+
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_ANON_KEY
+    );
